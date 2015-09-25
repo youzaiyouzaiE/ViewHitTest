83f87946ca2c44af337a17e1b63d3e4c9a2be018
Jiahui <goodzhoujiahui@gmail.com>
2015-09-25 15:22:22 +0800

    Initial Commit

---
+++ ViewHitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewHitTest
+//
+//  Created by jiahui on 15/9/25.
+//  Copyright © 2015年 YouZai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewHitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewHitTest
+//
+//  Created by jiahui on 15/9/25.
+//  Copyright © 2015年 YouZai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
